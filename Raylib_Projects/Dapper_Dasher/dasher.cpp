6cf8d396950796c9dbcb57fec2262d8a53accda1
george-mitchell <g.mitchell31@live.com>
2022-01-28 03:41:54 -0500

    used arrays

--- Raylib_Projects/Dapper_Dasher/dasher.cpp
+++ Raylib_Projects/Dapper_Dasher/dasher.cpp
@@ -17,12 +17,14 @@ struct AnimData
 
 int main() 
 {
-    // window dimensions
-    const int WindowWidth {512}; 
-    const int WindowHeight {380}; 
+    // window dimensions in an array
+    int WindowDimensions[2]; 
+    WindowDimensions[0] = 512;
+    WindowDimensions[1] = 380;
+
 
     // begin window
-    InitWindow(WindowWidth, WindowHeight, "Dapper Dasher"); 
+    InitWindow(WindowDimensions[0], WindowDimensions[1], "Dapper Dasher"); 
 
     // Initialize velocity measured in pixels per frame (p/f)
     int Velocity {0};      
@@ -37,7 +39,7 @@ int main()
     // create nebula animations with initializations
     AnimData NebulaData {
         {0.0, 0.0, Nebula.width/8, Nebula.height/8},    // rectangle rec
-        {WindowWidth, WindowHeight - Nebula.height/8.0},    // vector2 pos
+        {WindowDimensions[0], WindowDimensions[1] - Nebula.height/8.0},    // vector2 pos
         0,      // int frame
         1.0/12.0,   // float update time
         0.0       // float running time
@@ -46,18 +48,21 @@ int main()
 
     AnimData Nebula2Data {
         {0.0, 0.0, Nebula.width/8, Nebula.height/8}, 
-        {WindowWidth + 300, WindowHeight - Nebula.height/8.0}, 
+        {WindowDimensions[0] + 300, WindowDimensions[1] - Nebula.height/8.0}, 
         0, 
         1.0/16.0, 
         0.0
     };
+
+    // create array of nebulae
+    AnimData nebulae[2] {NebulaData, Nebula2Data};
 
 
     // Create scarfy character, including loading the sprites of scarfy from textures
     Texture2D Scarfy = LoadTexture("textures/scarfy.png");      // load the scarfy texture
     AnimData ScarfyData {
         {0,0, Scarfy.width/6, Scarfy.height}, 
-        {WindowWidth/2 - Scarfy.width/12, WindowHeight - Scarfy.height},
+        {WindowDimensions[0]/2 - Scarfy.width/12, WindowDimensions[1] - Scarfy.height},
         0,
         1.0/12.0,
         0.0
@@ -91,38 +96,38 @@ int main()
 
         // update RunningTimes with delta time to make it frame independent
         ScarfyData.RunningTime += dT; 
-        NebulaData.RunningTime += dT; 
+        nebulae[0].RunningTime += dT; 
 
         // update nebula position to make it frame rate independent
-        NebulaData.Pos.x += NebulaVelocity*dT; 
+        nebulae[0].Pos.x += NebulaVelocity*dT; 
 
         // update second nebula 
-        Nebula2Data.Pos.x += NebulaVelocity*dT; 
+        nebulae[1].Pos.x += NebulaVelocity*dT; 
         
         // update scarfy position with gravity taking in account
         ScarfyData.Pos.y += Velocity*dT; 
 
         // update nebula animation frame
-        if (NebulaData.RunningTime >= NebulaData.UpdateTime)
+        if (nebulae[0].RunningTime >= nebulae[0].UpdateTime)
         {
-            NebulaData.RunningTime = 0; 
-            NebulaData.Rec.x = NebulaData.frame * NebulaData.Rec.width; 
-            NebulaData.frame++; 
-            if (NebulaData.frame > 7)
+            nebulae[0].RunningTime = 0; 
+            nebulae[0].Rec.x = nebulae[0].frame * nebulae[0].Rec.width; 
+            nebulae[0].frame++; 
+            if (nebulae[0].frame > 7)
             {
-                NebulaData.frame = 0;
+                nebulae[0].frame = 0;
             }
         }
 
         // update nebula2 animation frame
-        if (Nebula2Data.RunningTime >= Nebula2Data.UpdateTime)
+        if (nebulae[1].RunningTime >= nebulae[1].UpdateTime)
         {
-            Nebula2Data.RunningTime = 0; 
-            Nebula2Data.Rec.x = Nebula2Data.frame * Nebula2Data.Rec.width; 
-            Nebula2Data.frame++; 
-            if (Nebula2Data.frame > 7)
+            nebulae[1].RunningTime = 0; 
+            nebulae[1].Rec.x = nebulae[1].frame * nebulae[1].Rec.width; 
+            nebulae[1].frame++; 
+            if (nebulae[1].frame > 7)
             {
-                Nebula2Data.frame = 0;
+                nebulae[1].frame = 0;
             }
         }
 
@@ -140,7 +145,7 @@ int main()
         }
 
         // This is the ground check. We prevent performing double jump with IsInAir
-        if (ScarfyData.Pos.y < WindowHeight - Scarfy.height)
+        if (ScarfyData.Pos.y < WindowDimensions[1] - Scarfy.height)
         {
             Velocity += Gravity*dT;        // rectangle is in the air, multiply by dT to convert to per second, not per frame
             IsInAir = true;
@@ -160,10 +165,10 @@ int main()
         
         
         // draw Nebula texture
-        DrawTextureRec(Nebula, NebulaData.Rec, NebulaData.Pos, WHITE);
+        DrawTextureRec(Nebula, nebulae[0].Rec, nebulae[0].Pos, WHITE);
 
         //draw second nebula
-        DrawTextureRec(Nebula, Nebula2Data.Rec, Nebula2Data.Pos, RED); 
+        DrawTextureRec(Nebula, nebulae[1].Rec, nebulae[1].Pos, RED); 
         
         // draw scarfy based on frame from sprite sheet
         DrawTextureRec(Scarfy, ScarfyData.Rec, ScarfyData.Pos, WHITE);
